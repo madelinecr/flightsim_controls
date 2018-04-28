f2a4e8ad8f2dccffc097191c908ba880a95387c5
Blaine Pace <blainepace@gmail.com>
2018-04-28 06:53:33 -0700

    Add proof of concept joystick code
    Add code that sends either a 0 or 1 over serial, based on pin 2's state. This state is translated over to button 3 on the gamepad controller section of Windows settings.

--- flightsim_controls.ino
+++ flightsim_controls.ino
@@ -1,16 +1,21 @@
-int inputPin = 2;
+#include <Joystick.h>
+
+int gameButton1 = 2;
+int pin1 = 2;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(inputPin, INPUT);
+  pinMode(pin1, INPUT);
   Serial.begin(9600);
+  Joystick.begin(true);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  int val = digitalRead(inputPin);
+  int val = digitalRead(pin1);
 
+  Joystick.setButton(gameButton1, val);
   Serial.print(val);
   delay(500);
 }
